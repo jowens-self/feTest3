ea1232ed19c1d814e3e9bbfdbf93d0c13744d09b
Jared <60214343+jowens-self@users.noreply.github.com>
2022-09-21 19:02:27 -0500

    testing component routing - need to relearn

---
+++ src/app/Components/home-screen/home-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-screen',
+  templateUrl: './home-screen.component.html',
+  styleUrls: ['./home-screen.component.css']
+})
+export class HomeScreenComponent implements OnInit {
+  title = 'feTest3';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
